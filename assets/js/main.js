de68976b615f31df4837b1cbd8f0e5f96354254a
Adrian Crespo Ames <Adrian_Ames@outlook.com>
2021-11-04 20:20:48 -0500

    Landing Emprendimiento

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function () {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.jpg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.png");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.jpg");
+            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.png");
         }
     });
 
